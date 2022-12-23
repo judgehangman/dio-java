ffd42c6f03b32e6609cd981ff11a3f1bf7ec3940
judgehangman <judgehangman@gmail.com>
2022-12-23 02:13:56 -0300

    Reestrura pastas + resolve exercício OOP

---
+++ exercicios resolvidos/orientacao-objetos/src/com/github/judgehangman/poo/Carro.java
@@ -0,0 +1,55 @@
+package com.github.judgehangman.poo;
+
+class Carro{
+
+    /* Aqui fica o código da classe */
+    String cor;
+    String modelo;
+    int capacidadeTanque;
+
+    // Construtores
+
+    Carro(){
+
+
+    }
+
+    Carro(String cor, String modelo, int capacidadeTanque){
+        this.cor = cor;
+        this.modelo = modelo;
+        this.capacidadeTanque = capacidadeTanque;
+    }
+
+    void setCor(String cor){
+        this.cor = cor;
+    }
+
+    String getCor(){
+        return cor;
+    }
+
+    void setModelo(String modelo){
+        this.modelo = modelo;
+    }
+
+    String getModelo(){
+        return modelo;
+    }
+
+    void setCapacidadeTanque(int capacidadeTanque){
+        this.capacidadeTanque = capacidadeTanque;
+    }
+
+    int getCapacidadeTanque(){
+        return capacidadeTanque;
+    }
+
+    // Método funcional
+
+    double calcValorTotal(double valorGasolina){
+        return capacidadeTanque * valorGasolina;
+    }
+
+
+
+}
